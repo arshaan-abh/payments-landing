c41d237a140b5ab37787f055e271ea42062f124a
arshaan-abh <arshaan.git@gmail.com>
2023-11-08 17:03:08 +0330

    Minor fix

--- app/_components/grid/index.tsx
+++ app/_components/grid/index.tsx
@@ -4,10 +4,10 @@ import PlusIcon from "../plus-icon";
 interface GridProps {}
 
 const Grid: FC<GridProps> = ({}) => {
-	const x = 13;
-	const y = 11;
+	const x = 12;
+	const y = 6;
 	return (
-		<div className="flex flex-col justify-between w-full h-full">
+		<div className="flex flex-col justify-evenly w-full h-full">
 			{Array.from(Array(y), (_, yIndex) => (
 				<div key={yIndex} className="flex justify-between">
 					{Array.from(Array(x), (_, xIndex) => (
