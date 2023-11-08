a5fad9f9eedbcd55baa3e71fea53bcd9f28e4e78
arshaan-abh <arshaan.git@gmail.com>
2023-11-08 15:54:11 +0330

    Add className prop to stack component

--- app/_components/stack/index.tsx
+++ app/_components/stack/index.tsx
@@ -4,13 +4,19 @@ interface StackProps {
 	children?: ReactNode;
 	underLayer?: ReactNode;
 	overLayer?: ReactNode;
+	className?: string;
 }
 
-const Stack: FC<StackProps> = ({ children, overLayer, underLayer }) => {
+const Stack: FC<StackProps> = ({
+	children,
+	overLayer,
+	underLayer,
+	className = "",
+}) => {
 	return (
 		<div className="relative">
 			<div className="absolute z-0 inset-0">{underLayer}</div>
-			<div className="relative z-10">{children}</div>
+			<div className={`relative z-10 ${className}`}>{children}</div>
 			<div className="absolute z-20 inset-0 pointer-events-none">
 				{overLayer}
 			</div>
