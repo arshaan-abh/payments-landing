bc90359e8634dc9c248a6e8b76ae3c21eee7e839
arshaan-abh <arshaan.git@gmail.com>
2023-11-13 16:53:17 +0330

    Add plane plus icon

--- app/_components/icons/index.tsx
+++ app/_components/icons/index.tsx
@@ -189,3 +189,18 @@ export const CheckIcon: FC<IconProps> = ({ className }) => {
 		</svg>
 	);
 };
+
+export const PlanePlusIcon: FC<IconProps> = ({ className }) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			fill="currentColor"
+			className={className}
+		>
+			<path d="M10 0h4v24h-4V0z" />
+			<path d="M0 14v-4h24v4H0z" />
+		</svg>
+	);
+};
