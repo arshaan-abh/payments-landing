6018ba0a174c4c777b9e6e1389fcfebf03c08f19
Arshaan <arshan@selected.org>
2023-11-20 15:19:53 +0330

    Make menu buttons static sized

--- app/_components/menu/index.tsx
+++ app/_components/menu/index.tsx
@@ -39,13 +39,13 @@ interface MenuItemProps {
 }
 
 const MenuItem: FC<MenuItemProps> = ({ primary, children }) => {
-	const w = primary ? "w-16" : "w-11";
+	const w = primary ? "w-[64px]" : "w-[44px]";
 	return (
 		<div
 			className={`${w} text-white aspect-square bg-white/10 rounded-full backdrop-blur-sm flex items-center justify-center`}
 		>
 			{primary ? (
-				<div className="w-12 aspect-square bg-white rounded-full text-primary flex justify-center items-center">
+				<div className="w-[48px] aspect-square bg-white rounded-full text-primary flex justify-center items-center">
 					{children}
 				</div>
 			) : (
