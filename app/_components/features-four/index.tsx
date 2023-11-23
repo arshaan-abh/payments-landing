d06517f78ade0c49c0d91473cf1cd093ded59dc2
Arshaan <arshan@selected.org>
2023-11-23 15:01:43 +0330

    Add a breakpoint

--- app/_components/features-four/index.tsx
+++ app/_components/features-four/index.tsx
@@ -34,12 +34,16 @@ const FeatureFour: FC<FeatureFourProps> = ({}) => {
 				<h3 className="text-primary text-4xl font-extrabold text-center lg:text-start">
 					Empowering Effortless Money Transfers
 				</h3>
-				<p className="font-medium text-base text-gray-200 text-center lg:text-start">
-					Our platform is your gateway to effortless financial transactions. We
-					provide user-friendly, secure, and hassle-free money transfer
-					solutions, so you can efficiently manage your finances. Join us today
-					and discover a new level of financial convenience.
-				</p>
+				<div className="font-medium text-base text-gray-200 text-center lg:text-start">
+					<p className="inline">
+						Our platform is your gateway to effortless financial transactions.
+					</p>
+					<p className="hidden sm:inline">
+						&nbsp;We provide user-friendly, secure, and hassle-free money
+						transfer solutions, so you can efficiently manage your finances.
+						Join us today and discover a new level of financial convenience.
+					</p>
+				</div>
 				<Hoverable round>
 					<Image src={Empowering} alt="Empowering" />
 				</Hoverable>
@@ -58,12 +62,14 @@ interface CardProps {
 
 const Card: FC<CardProps> = ({ title, supportingText, percent }) => {
 	return (
-		<div className="flex flex-col gap-2 w-[48%]">
-			<div className="font-extrabold text-primary text-6xl lg:text-7xl">
+		<div className="flex flex-col gap-2 w-full sm:w-[48%]">
+			<div className="font-extrabold text-primary text-6xl lg:text-7xl text-center sm:text-start">
 				{title}
 			</div>
 			<div className="h-5 bg-gray-700" style={{ width: `${percent}%` }} />
-			<p className="font-medium text-base text-gray-100">{supportingText}</p>
+			<p className="font-medium text-base text-gray-100 text-center sm:text-start">
+				{supportingText}
+			</p>
 		</div>
 	);
 };
