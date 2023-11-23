06512922dd1a8731db12850562c95610d15974bd
Arshaan <arshan@selected.org>
2023-11-23 15:56:29 +0330

    Make section five and six responsive

--- app/_components/features-five/index.tsx
+++ app/_components/features-five/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, type FC, useRef, useEffect, useCallback } from "react";
+import { useState, type FC, useRef, ReactNode } from "react";
 import Pre from "public/credit-card.jpg";
 import Business from "public/business-finance.jpg";
 import { HoverableImage } from "../hoverable";
@@ -41,37 +41,49 @@ const FeatureFive: FC<FeatureFiveProps> = ({}) => {
 		<div className="w-full h-[calc(100vh_-_4rem)] px-10 lg:px-20 xl:px-30 xl:max-w-screen-1.5xl flex flex-col">
 			<div className="grow-2 min-h-[1rem]" />
 			<div
-				className="flex items-center gap-16 transition-all justify-between"
-				style={{ height: `${biggerHeight}%` }}
+				className="flex items-center gap-8 xl:gap-16 transition-all xl:justify-between flex-col xl:flex-row xl:h-1/2"
 				ref={biggerElementRef}
 			>
 				<HoverableImage
 					src={Pre}
 					alt="Pre"
-					className="h-full w-max aspect-square object-cover"
+					className="xl:h-full w-1/2 xl:w-max xl:aspect-square object-cover"
 					round
 				/>
 
-				<Card
-					title="Pre-Paid Debit Card Issuing with Bank Account"
-					supportingText="By easily issuing Prepaid Debit Cards tailored to your brand, you create a distinct competitive edge in the market. These adaptable solutions not only optimise payments but also empower your business to provide a unique payment experience that's both user-friendly and reflective of your brand's values. This uniqueness resonates with customers, enhancing their engagement and loyalty. Moreover, the introduction of these cards can open up new revenue streams, as satisfied customers are more likely to increase their spending and recommend your business to others. As you align your payment options with your brand identity, you're poised for business growth and long-term success."
-				/>
+				<Card title="Pre-Paid Debit Card Issuing with Bank Account">
+					<p className="inline">
+						By easily issuing Prepaid Debit Cards tailored to your brand, you
+						create a distinct competitive edge in the market.
+					</p>
+					<p className="hidden xl:inline">
+						&nbsp;These adaptable solutions not only optimise payments but also
+						empower your business to provide a unique payment experience
+						that&apos;s both user-friendly and reflective of your brand&apos;s
+						values. This uniqueness resonates with customers, enhancing their
+						engagement and loyalty. Moreover, the introduction of these cards
+						can open up new revenue streams, as satisfied customers are more
+						likely to increase their spending and recommend your business to
+						others. As you align your payment options with your brand identity,
+						you&apos;re poised for business growth and long-term success.
+					</p>
+				</Card>
 			</div>
 			<div className="grow min-h-[1rem]" />
 			<div
-				className="flex items-center gap-16 transition-all justify-between"
-				style={{ height: `${smallerHeight}%` }}
+				className="flex items-center xl:gap-16 gap-8 transition-all xl:justify-between flex-col-reverse xl:flex-row xl:h-1/4"
 				ref={smallerElementRef}
 			>
-				<Card
-					title="Business Finance Service with Bank Account"
-					supportingText="We've tailored our services to provide you with a seamless experience in managing your business's credit lines and bank accounts."
-				/>
+				<Card title="Business Finance Service with Bank Account">
+					We&apos;ve tailored our services to provide you with a seamless
+					experience in managing your business&apos;s credit lines and bank
+					accounts.
+				</Card>
 
 				<HoverableImage
 					src={Business}
 					alt="Business"
-					className="h-full w-max aspect-square object-cover"
+					className="xl:h-full w-1/2 xl:w-max xl:aspect-square object-cover"
 					round
 					locate
 				/>
@@ -85,14 +97,18 @@ export default FeatureFive;
 
 interface CardProps {
 	title: string;
-	supportingText: string;
+	children?: ReactNode;
 }
 
-export const Card: FC<CardProps> = ({ title, supportingText }) => {
+export const Card: FC<CardProps> = ({ title, children }) => {
 	return (
 		<div className="flex flex-col gap-4">
-			<h4 className="font-extrabold text-3xl text-primary">{title}</h4>
-			<p className="font-normal text-base text-gray-200">{supportingText}</p>
+			<h4 className="font-extrabold text-3xl text-primary text-center xl:text-start">
+				{title}
+			</h4>
+			<div className="font-normal text-base text-gray-200 text-center xl:text-start">
+				{children}
+			</div>
 		</div>
 	);
 };
--- app/_components/features-six/index.tsx
+++ app/_components/features-six/index.tsx
@@ -10,29 +10,41 @@ const FeatureFive: FC<FeatureFiveProps> = ({}) => {
 	return (
 		<div className="w-full h-[calc(100vh_-_4rem)] px-10 lg:px-20 xl:px-30 xl:max-w-screen-1.5xl flex flex-col">
 			<div className="grow-2 min-h-[1rem]" />
-			<div className="flex items-center h-2/4 gap-16 justify-between">
+			<div className="flex items-center gap-8 xl:gap-16 xl:justify-between flex-col xl:flex-row xl:h-1/2">
 				<HoverableImage
 					src={Advance}
 					alt="Advance"
-					className="h-full object-cover aspect-square w-max"
+					className="xl:h-full w-1/2 object-cover xl:aspect-square xl:w-max"
 					round
 				/>
-				<Card
-					title="Advanced Terminal Services"
-					supportingText="For businesses in search of the pinnacle of terminal technology, our solutions stand out as the optimal choice.
-					It is meticulously crafted to elevate in-person transactions to the highest standard of sophistication. With an unwavering commitment to security, speed, and reliability, you can rest assured that your payment processes are not only smooth but also held in the highest regard for trustworthiness. In an era where customers prioritise a secure payment experience, our solutions go above and beyond, guaranteeing that your business maintains its competitive edge and continues to be trusted by your valued clientele for unparalleled excellence and innovation."
-				/>
+				<Card title="Advanced Terminal Services">
+					<p className="inline">
+						For businesses in search of the pinnacle of terminal technology, our
+						solutions stand out as the optimal choice.
+					</p>
+					<p className="hidden xl:inline">
+						&nbsp;It is meticulously crafted to elevate in-person transactions
+						to the highest standard of sophistication. With an unwavering
+						commitment to security, speed, and reliability, you can rest assured
+						that your payment processes are not only smooth but also held in the
+						highest regard for trustworthiness. In an era where customers
+						prioritise a secure payment experience, our solutions go above and
+						beyond, guaranteeing that your business maintains its competitive
+						edge and continues to be trusted by your valued clientele for
+						unparalleled excellence and innovation.
+					</p>
+				</Card>
 			</div>
 			<div className="grow min-h-[1rem]" />
-			<div className="flex items-center h-1/4 gap-16 justify-between">
-				<Card
-					title="Billing & Invoicing"
-					supportingText="One of the key features of our Automated Billing solutions is the ability to craft professional invoices."
-				/>
+			<div className="flex items-center xl:gap-16 gap-8 xl:justify-between flex-col-reverse xl:flex-row xl:h-1/4">
+				<Card title="Billing & Invoicing">
+					One of the key features of our Automated Billing solutions is the
+					ability to craft professional invoices.
+				</Card>
 				<HoverableImage
 					src={Billing}
 					alt="Billing"
-					className="h-full object-cover aspect-square w-max"
+					className="xl:h-full w-1/2 object-cover xl:aspect-square xl:w-max"
 					round
 					locate
 				/>
