6c78956530ec8d85709f72ddfa0d3d33e419dc63
arshaan-abh <arshaan.git@gmail.com>
2023-11-26 12:02:59 +0330

    Make form section responsive

--- app/_components/button/index.tsx
+++ app/_components/button/index.tsx
@@ -1,18 +1,25 @@
 "use client";
-import type { FC, MouseEventHandler, ReactNode } from "react";
+import type { FC, MouseEventHandler, ReactNode, RefObject } from "react";
 
 interface ButtonProps {
+	customRef?: RefObject<HTMLButtonElement>;
 	children?: ReactNode;
 	white?: boolean;
 	clickHandler?: MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button: FC<ButtonProps> = ({ children, white, clickHandler }) => {
+const Button: FC<ButtonProps> = ({
+	children,
+	white,
+	clickHandler,
+	customRef,
+}) => {
 	const variableClassNames = white
 		? "text-primary bg-white"
 		: "text-white bg-primary";
 	return (
 		<button
+			ref={customRef}
 			onClick={clickHandler}
 			className={`${variableClassNames} py-3 px-8 rounded-md text-base font-bold whitespace-nowrap`}
 		>
--- app/_components/form/index.tsx
+++ app/_components/form/index.tsx
@@ -7,6 +7,8 @@ import { EnquireRequest } from "@/app/@types/enquireRequest";
 import FormSection from "./form";
 import Checkbox from "./Checkbox";
 import Button from "../button";
+import useSlider from "../features-seven/Slider";
+import useResponsiveState from "../features-one/useResponsiveState";
 const schema = object({
 	name: string().required("This field is required."),
 	companyName: string().required("This field is required."),
@@ -77,6 +79,51 @@ function Form() {
 			});
 	};
 
+	const visibleSlidesNumber = useResponsiveState({
+		defaultState: 1,
+		breakpoints: [{ breakpoint: 768, state: 2 }],
+	});
+
+	const gapInRem = useResponsiveState({
+		defaultState: 0,
+		breakpoints: [{ breakpoint: 768, state: 2.5 }],
+	});
+
+	const [slider, _, nextButtonRef] = useSlider({
+		children: [
+			<div className="flex flex-col basis-1/2" key={0}>
+				<p className="text-xl text-secondary font-extrabold text-center mb-4">
+					Select your favorite solutions
+				</p>
+				<div className="flex flex-wrap gap-3 mb-3">
+					{options.map((option, index) => {
+						const handler = () => {
+							checkHandler(index);
+						};
+						return (
+							<Checkbox checkHandler={handler} key={index}>
+								{option}
+							</Checkbox>
+						);
+					})}
+				</div>
+				{checkableList[11] && (
+					<textarea
+						id="message"
+						rows={3}
+						disabled={checkableList[11] ? false : true}
+						className="w-full text-base font-semibold text-secondary border-2 border-gray-100 rounded-md p-2.5 bg-transparent focus:border-secondary"
+						placeholder="Others"
+						style={{ resize: "none" }}
+					></textarea>
+				)}
+			</div>,
+			<FormSection control={control} errors={errors} key={1} />,
+		],
+		gapInRem,
+		visibleSlidesNumber,
+	});
+
 	return (
 		<div className="w-full h-full px-10 lg:px-20 xl:px-30 xl:max-w-screen-1.5xl flex flex-col items-center">
 			<div className="grow min-h-[1rem]" />
@@ -86,37 +133,14 @@ function Form() {
 			<p className="text-lg font-medium text-gray-400 mb-8">
 				Elevate your business with our convenient in-app payment solutions.
 			</p>
-			<div className="flex w-full justify-evenly gap-16 mb-2.5">
-				<div className="flex flex-col basis-1/2">
-					<p className="text-xl text-secondary font-extrabold text-center mb-4">
-						Select your favorite solutions
-					</p>
-					<div className="flex flex-wrap gap-3 mb-3">
-						{options.map((option, index) => {
-							const handler = () => {
-								checkHandler(index);
-							};
-							return (
-								<Checkbox checkHandler={handler} key={index}>
-									{option}
-								</Checkbox>
-							);
-						})}
-					</div>
-					{checkableList[11] && (
-						<textarea
-							id="message"
-							rows={3}
-							disabled={checkableList[11] ? false : true}
-							className="w-full text-base font-semibold text-secondary border-2 border-gray-100 rounded-md p-2.5 bg-transparent focus:border-secondary"
-							placeholder="Others"
-							style={{ resize: "none" }}
-						></textarea>
-					)}
-				</div>
-				<FormSection control={control} errors={errors} />
-			</div>
-			<Button clickHandler={handleFormSubmit} white>
+			<div className="flex mb-2.5">{slider}</div>
+			<Button
+				customRef={nextButtonRef}
+				clickHandler={() => {
+					handleFormSubmit();
+				}}
+				white
+			>
 				Enquire Now
 			</Button>
 			<div className="grow-2 min-h-[1rem]" />
