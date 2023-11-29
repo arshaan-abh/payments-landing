1e96cc5ff4f2abd8d6f5a28994a20ad1b9e5a1bf
Arshaan <arshan@selected.org>
2023-11-29 17:32:25 +0330

    Adjust responsive

--- app/_components/features-one/index.tsx
+++ app/_components/features-one/index.tsx
@@ -4,12 +4,10 @@ import React, { FC, ReactNode } from "react";
 import featureImageOne from "../../../public/one-click.jpg";
 import featureImageTwo from "../../../public/pay-method.jpg";
 import featureImageThree from "../../../public/currency.jpg";
-import Stack from "../stack";
 import useSlider from "../features-seven/Slider";
 import useResponsiveState from "./useResponsiveState";
 import { SlideButton } from "../features-seven";
 import { ArrowLeftIcon, ArrowRightIcon } from "../icons";
-import Hoverable from "../hoverable";
 
 const FeatureOne: FC<CardProps> = ({}) => {
 	const visibleSlidesNumber = useResponsiveState<number>({
@@ -25,12 +23,7 @@ const FeatureOne: FC<CardProps> = ({}) => {
 			<Card
 				key={0}
 				media={
-					<Hoverable round>
-						<Image
-							src={featureImageOne}
-							alt="One-Click checkout with Pay link"
-						/>
-					</Hoverable>
+					<Image src={featureImageOne} alt="One-Click checkout with Pay link" />
 				}
 				supportingText="Your customers will love the simplicity of one-click checkout through our seamless pay link."
 			>
@@ -38,22 +31,14 @@ const FeatureOne: FC<CardProps> = ({}) => {
 			</Card>,
 			<Card
 				key={1}
-				media={
-					<Hoverable round>
-						<Image src={featureImageTwo} alt="Flexible Payment Methods" />
-					</Hoverable>
-				}
+				media={<Image src={featureImageTwo} alt="Flexible Payment Methods" />}
 				supportingText="Users will enjoy the convenience of flexible payout methods tailored to their needs."
 			>
 				Flexible Payment Methods
 			</Card>,
 			<Card
 				key={2}
-				media={
-					<Hoverable round>
-						<Image src={featureImageThree} alt="Multicurrency Payments" />
-					</Hoverable>
-				}
+				media={<Image src={featureImageThree} alt="Multicurrency Payments" />}
 				supportingText="Clients will recognise the benefits of adaptable multi-currency payment options."
 			>
 				Multicurrency Payments
@@ -64,18 +49,13 @@ const FeatureOne: FC<CardProps> = ({}) => {
 	});
 
 	return (
-		<Stack
-			containerClassName="w-full h-full px-10 lg:px-20 xl:px-30 xl:max-w-screen-1.5xl"
-			// underClassName="px-10 lg:px-20 xl:px-30 xl:max-w-screen-1.5xl"
-			className="flex h-full flex-col py-4"
-			// underLayer={<Grid black />}
-		>
-			<div className="min-h-[1rem] grow-2" />
-			<div className="flex flex-col items-center gap-x-16 gap-y-2 md:flex-row">
-				<div className="text-center text-4xl font-extrabold text-primary md:w-1/2 md:text-left">
+		<div className="flex h-full w-full flex-col px-10 py-4 lg:px-20 xl:max-w-screen-1.5xl xl:px-30">
+			<div className="min-h-[1rem] grow" />
+			<div className="flex flex-col items-center gap-x-8 gap-y-2 md:flex-row">
+				<div className="text-center text-4xl font-extrabold leading-tight text-primary md:w-1/2 md:text-left">
 					Simplified Customer Payment Processing!
 				</div>
-				<div className="text-center text-base font-medium text-gray-200 md:w-1/2 md:text-left">
+				<div className="text-center text-base font-medium leading-tight text-gray-200 md:w-1/2 md:text-left">
 					<p className="inline">
 						In a rapidly evolving marketplace, staying ahead means embracing
 						efficient solutions.
@@ -87,7 +67,7 @@ const FeatureOne: FC<CardProps> = ({}) => {
 					</p>
 				</div>
 			</div>
-			<div className="min-h-[1rem] grow" />
+			<div className="min-h-[1rem] grow-2" />
 			<div className="flex flex-col gap-8">
 				{slider}
 				{visibleSlidesNumber < 3 && (
@@ -105,7 +85,7 @@ const FeatureOne: FC<CardProps> = ({}) => {
 				)}
 			</div>
 			<div className="min-h-[1rem] grow-2" />
-		</Stack>
+		</div>
 	);
 };
 
@@ -119,16 +99,73 @@ interface CardProps {
 
 const Card: FC<CardProps> = ({ media, children, supportingText }) => {
 	return (
-		<div className="flex basis-1/3 flex-col items-center">
-			<div className="mb-4 w-2/3 xs:w-auto">{media}</div>
-			<div className="mb-2 w-full text-center text-2xl font-extrabold text-primary sm:text-start">
-				{children}
+		<div className="flex basis-1/3 flex-col items-center hover:[--border:50%] hover:[--transform:scale(1.1)_rotate(4deg)]">
+			<div className="relative w-2/3 p-4 xs:w-auto lg:p-7">
+				<div className="overflow-hidden">
+					<div className="transition-transform duration-300 [transform:var(--transform)]">
+						{media}
+					</div>
+				</div>
+				<GridBorder />
 			</div>
-			<div className="w-full text-center text-base font-medium text-gray-200 sm:text-start">
-				{supportingText}
+			<div className="relative p-4 lg:p-7">
+				<div className="mb-2 w-full text-center text-xl font-extrabold text-primary sm:text-start">
+					{children}
+				</div>
+				<div className="w-full text-center text-base font-medium text-gray-200 sm:text-start">
+					{supportingText}
+				</div>
+				<GridBorder noBorderTop />
 			</div>
 		</div>
 	);
 };
 
 // TODO inspect image component's props
+
+interface GridBorderProps {
+	noBorderTop?: boolean;
+}
+
+const GridBorder: FC<GridBorderProps> = ({ noBorderTop }) => {
+	return (
+		<>
+			{noBorderTop ?? (
+				<>
+					<div
+						className="absolute right-0 top-0 h-0.5 bg-gray-600 transition-all duration-300"
+						style={{ width: "var(--border, 0.5rem)" }}
+					/>
+					<div
+						className="absolute left-0 top-0 h-0.5 bg-gray-600 transition-all duration-300"
+						style={{ width: "var(--border, 0.5rem)" }}
+					/>
+				</>
+			)}
+			<div
+				className="absolute left-0 top-0 w-0.5 bg-gray-600 transition-all duration-300"
+				style={{ height: "var(--border, 0.5rem)" }}
+			/>
+			<div
+				className="absolute right-0 top-0 w-0.5 bg-gray-600 transition-all duration-300"
+				style={{ height: "var(--border, 0.5rem)" }}
+			/>
+			<div
+				className="absolute bottom-0 right-0 h-0.5 bg-gray-600 transition-all duration-300"
+				style={{ width: "var(--border, 0.5rem)" }}
+			/>
+			<div
+				className="absolute bottom-0 right-0 w-0.5 bg-gray-600 transition-all duration-300"
+				style={{ height: "var(--border, 0.5rem)" }}
+			/>
+			<div
+				className="absolute bottom-0 left-0 h-0.5 bg-gray-600 transition-all duration-300"
+				style={{ width: "var(--border, 0.5rem)" }}
+			/>
+			<div
+				className="absolute bottom-0 left-0 w-0.5 bg-gray-600 transition-all duration-300"
+				style={{ height: "var(--border, 0.5rem)" }}
+			/>
+		</>
+	);
+};
