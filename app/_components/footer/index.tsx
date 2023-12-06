9535c91aaf8ffde9e297cbca856177ba207199cf
Arshaan <arshan@selected.org>
2023-12-06 12:04:37 +0330

    Redesign footer

--- app/_components/footer/index.tsx
+++ app/_components/footer/index.tsx
@@ -1,11 +1,11 @@
 import type { FC } from "react";
 import Image from "next/image";
-import logo from "public/logo.svg";
+import logo from "public/logo-white.svg";
 import Facebook from "../../../public/facebook.svg";
-import Twitter from "../../../public/twitter-x-fill.svg";
-import Youtube from "../../../public/youtube-line.svg";
-import Linkedin from "../../../public/linkedin-fill.svg";
-import InstagramIcon from "../../../public/instagram-fill.svg";
+import Twitter from "../../../public/twitter.svg";
+import Youtube from "../../../public/youtube.svg";
+import Linkedin from "../../../public/linkedin.svg";
+import InstagramIcon from "../../../public/instagram.svg";
 import Phone from "../../../public/phone.svg";
 import Email from "../../../public/email.svg";
 import Location from "../../../public/location.svg";
@@ -20,8 +20,8 @@ const solutionsLinks = [
 	{ text: "In-Person Payment", link: "" },
 	{ text: "Terminal-less Payments ( Mobile App )", link: "" },
 	{ text: "ePOS All-In-One ( Terminal + eposense mobile ) ", link: "" },
-	{ text: "Pre- Paid Debit Card with Bank Account issuing ", link: "" },
-	{ text: "Business’s Credit adn Bank Account Issuing ", link: "" },
+	{ text: "Pre-Paid Debit Card with Bank Account issuing ", link: "" },
+	{ text: "Business's Credit adn Bank Account Issuing ", link: "" },
 ];
 const featuresLinks = [
 	{ text: "Instant Settlement", link: "" },
@@ -41,39 +41,39 @@ const Footer: FC<FooterProps> = ({}) => {
 	return (
 		<div className="flex h-full w-full flex-col">
 			<div className="grow"></div>
-			<div className="flex justify-center bg-primary-950 text-secondary-950">
-				<div className="flex flex-col gap-11 px-10 pt-11 lg:px-20 xl:max-w-screen-1.5xl xl:px-30">
+			<div className="flex justify-center bg-primary-950 text-white">
+				<div className="flex flex-col gap-9 px-10 pt-11 lg:px-20 xl:max-w-screen-1.5xl xl:px-30">
 					<Image src={logo} alt="Logo" />
 					<div className="flex flex-wrap justify-center gap-4 sm:flex-nowrap">
-						<div className="flex basis-full flex-col gap-3 text-start xs:basis-2/5 xs:text-center sm:basis-1/3 sm:text-start">
+						<div className="flex basis-full flex-col gap-2 text-start xs:basis-2/5 xs:text-center sm:basis-1/3 sm:text-start">
 							<h5 className="mb-1 text-base font-extrabold">Solutions</h5>
 							{solutionsLinks.map((link, index) => (
 								<Link
 									key={index}
 									href={link.link}
-									className="text-sm font-medium text-secondary-950"
+									className="text-sm font-medium text-white"
 								>
 									{link.text}
 								</Link>
 							))}
 						</div>
-						<div className="flex basis-full flex-col gap-3 text-start xs:basis-2/5 xs:text-center sm:basis-1/3 sm:text-start">
+						<div className="flex basis-full flex-col gap-2 text-start xs:basis-2/5 xs:text-center sm:basis-1/3 sm:text-start">
 							<h5 className="mb-1 text-base font-extrabold">Features</h5>
 							{featuresLinks.map((link, index) => (
 								<Link
 									key={index}
 									href={link.link}
-									className="text-sm font-medium text-secondary-950"
+									className="text-sm font-medium text-white"
 								>
 									{link.text}
 								</Link>
 							))}
 						</div>
-						<div className="flex basis-full flex-col gap-3 text-start xs:basis-3/5 xs:text-center sm:basis-1/3 sm:text-start">
+						<div className="flex basis-full flex-col gap-2 text-start xs:basis-3/5 xs:text-center sm:basis-1/3 sm:text-start">
 							<h5 className="mb-1 text-base font-extrabold">
 								We&apos;re social
 							</h5>
-							<p className="text-sm font-medium text-secondary-950">
+							<p className="text-sm font-medium text-white">
 								Follow us on social media and scroll through the latest scoop
 								effortlessly in your feed
 							</p>
@@ -87,15 +87,15 @@ const Footer: FC<FooterProps> = ({}) => {
 							<h5 className="mb-1 text-base font-extrabold">
 								Join us and stay ahead of the curve
 							</h5>
-							<p className="mb-2 text-sm font-medium text-secondary-950">
+							<p className="mb-2 text-sm font-medium text-white">
 								Get the latest news and exclusive promotions conveniently in
 								your inbox.
 							</p>
-							<div className="flex flex-row gap-1 sm:flex-col lg:flex-row">
+							<div className="flex flex-row gap-3 sm:flex-col lg:flex-row">
 								<input
 									type="email"
 									placeholder="Email"
-									className="w-full rounded-md border border-secondary-950 bg-[#ffffff0a] px-4 py-2 text-base font-normal text-secondary-950 outline-none"
+									className="w-full rounded-md border border-white bg-white/10 px-4 py-2 text-base font-normal text-white outline-none placeholder:text-white"
 								/>
 								<Button className="w-1/2 sm:w-full lg:w-1/2">Sign up</Button>
 							</div>
@@ -131,9 +131,9 @@ const Footer: FC<FooterProps> = ({}) => {
 						<div className="flex flex-col items-center justify-between gap-y-4 border-t border-gray-100 py-3 pt-6 md:flex-row md:pt-3">
 							<p className="text-xs font-normal text-gray-400">
 								Copyright © 2023 Powered by{" "}
-								<span className="font-bold text-secondary-950">Selected</span>
+								<span className="font-bold text-white">Selected</span>
 							</p>
-							<p className="text-xs font-bold text-secondary-950">
+							<p className="text-xs font-bold text-white">
 								Privacy Policy | Terms of Use
 							</p>
 						</div>
